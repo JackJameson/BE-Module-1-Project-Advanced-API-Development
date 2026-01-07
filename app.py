50f7a2caccd124771b247d47cfec44917a250876
James Jackman <jamesdjackman@yahoo.com>
2026-01-07 00:34:16 -0500

    add member, loan, and book tables

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/library_db'
@@ -13,6 +14,32 @@ db = SQLAlchemy(model_class=Base)
 
 db.init_app(app)
 
+class Member(Base):
+    __tablename__ = 'members'
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    email: Mapped[str] = mapped_column(db.String(360), nullable=False, unique=True)
+    DOB: Mapped[date]
+    password: Mapped[str] = mapped_column(db.String(255), nullable=False)
+
+class Loan(Base):
+    __tablename__ = 'loans'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    loan_date: Mapped[date] = mapped_column(db.Date)
+    member_id: Mapped[int] = mapped_column(db.ForeignKey('members.id'))
+    
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    genre: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    desc: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    title: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    
+with app.app_context():
+			db.create_all()
 
 app.run()
