3e9337827fe50118238e2f922c6ca82662054ead
James Jackman <jamesdjackman@yahoo.com>
2026-01-07 10:30:03 -0500

    fixed some issues

--- app.py
+++ app.py
@@ -15,44 +15,45 @@ db = SQLAlchemy(model_class=Base)
 
 db.init_app(app)
 
+service_mechanic = db.Table(
+    'service_mechanics',
+    Base.metadata,
+    db.Column('ticket_id', db.ForeignKey('service_tickets.id')),
+    db.Column('mechanic_id', db.ForeignKey('mechanics.id'))
+)
+
 class Customer(Base):
     __tablename__ = 'customers'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(255), nullable=False)
-    email: Mapped[str] = mapped_column(db.String(360), nullable=False, unique=True)
-    phone: Mapped[str] = mapped_column(db.String(10), nullable=False)
+    email: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
+    phone: Mapped[str] = mapped_column(db.String(20), nullable=False)
     service_tickets: Mapped[List['ServiceTicket']] = db.relationship(back_populates='customer')
-    
-service_mechanic = db.Table(
-    'service_mechanic',
-    Base.metadata,
-    db.Column('service_tickets_id', db.ForeignKey('service_tickets.id')),
-    db.Column('mechanic_id', db.ForeignKey('mechanics.id'))
-)
 
 class ServiceTicket(Base):
     __tablename__ = 'service_tickets'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    VIN: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    VIN: Mapped[str] = mapped_column(db.String(17), nullable=False)
     service_date: Mapped[date] = mapped_column(db.Date)
     service_desc: Mapped[str] = mapped_column(db.String(255), nullable=False)
-    member_id: Mapped[int] = mapped_column(db.ForeignKey('customer.id'))
-    member: Mapped['Customer'] = db.relationship(back_populates='service_tickets')
+    customer_id: Mapped[int] = mapped_column(db.ForeignKey('customers.id'))
+    customer: Mapped['Customer'] = db.relationship(back_populates='service_tickets')
     mechanics: Mapped[List['Mechanic']] = db.relationship(secondary=service_mechanic, back_populates='service_tickets')
-    
+
 class Mechanic(Base):
     __tablename__ = "mechanics"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(db.String(255), nullable=False)
     email: Mapped[str] = mapped_column(db.String(255), nullable=False)
-    phone: Mapped[str] = mapped_column(db.String(10), nullable=False)
+    phone: Mapped[str] = mapped_column(db.String(20), nullable=False)
     salary: Mapped[float] = mapped_column(db.Float, nullable=False)
     service_tickets: Mapped[List['ServiceTicket']] = db.relationship(secondary=service_mechanic, back_populates='mechanics')
-    
-with app.app_context():
-	db.create_all()
 
-app.run(debug=True)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
